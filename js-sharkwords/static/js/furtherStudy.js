57eaac657fd47e6f2d9bfa6fa10b36733d95d4b4
Jade Le <baongocle1991@gmail.com>
2020-10-30 03:51:38 +0000

    complete sharkwords game suing built-in  Javascript

--- js-sharkwords/static/js/furtherStudy.js
+++ js-sharkwords/static/js/furtherStudy.js
@@ -1,17 +1,17 @@
-const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 const WORDS = [
-  'strawberry',
-  'orange',
-  'apple',
-  'banana',
-  'pineapple',
-  'kiwi',
-  'peach',
-  'pecan',
-  'eggplant',
-  'durian',
-  'peanut',
-  'chocolate',
+  "strawberry",
+  "orange",
+  "apple",
+  "banana",
+  "pineapple",
+  "kiwi",
+  "peach",
+  "pecan",
+  "eggplant",
+  "durian",
+  "peanut",
+  "chocolate",
 ];
 
 let numWrong = 0;
@@ -20,12 +20,26 @@ let numWrong = 0;
 //
 const createDivsForChars = (word) => {
   // Replace this with your code
+  for (const letter of word) {
+    const wordContainer = document.querySelector("#word-container");
+    const div = document.createElement("div");
+    div.classList.add("letter-box");
+    div.classList.add(letter);
+    wordContainer.append(div);
+  }
 };
 
 // Loop over each letter in `ALPHABET` and generate buttons.
 //
 const generateLetterButtons = () => {
   // Replace this with your code
+  for (const letter of ALPHABET) {
+    const letterButton = document.querySelector("#letter-buttons");
+    const button = document.createElement("button");
+    button.classList.add(letter);
+    button.innerHTML = letter;
+    letterButton.append(button);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,18 +48,21 @@ const generateLetterButtons = () => {
 //
 const disableLetterButton = (buttonEl) => {
   // Replace this with your code
+  buttonEl.setAttribute('disabled', true);
 };
 
 // Return `true` if `letter` is in the word.
 //
 const isLetterInWord = (letter) => {
   // Replace this with your code
+  return document.querySelector(`div.${letter}`) !== null;
 };
 
 // Called when `letter` is in word. Update contents of divs with `letter`.
 //
 const handleCorrectGuess = (letter) => {
   // Replace this with your code
+  document.querySelector(`div.${letter}`).innerHTML = letter
 };
 
 // Called when `letter` is not in word.
@@ -55,24 +72,35 @@ const handleCorrectGuess = (letter) => {
 //
 const handleWrongGuess = () => {
   // Replace this with your code
+
+  numWrong += 1
+  document.querySelector('#shark-img img').setAttribute('src', `/static/images/guess${numWrong}.png`)
+
+  if (numWrong === 5) {
+    const buttons = document.querySelectorAll('buttons');
+    for (const btn of buttons) {
+      btn.setAttribute('disabled', true);
+    }
+    document.querySelector('#play-again').style.display = '';
+  }
 };
 
 //  Reset game state. Called before restarting the game.
 //
 const resetGame = () => {
-  window.location = '/sharkwords';
+  window.location = "/sharkwords";
 };
 
 // This is like if __name__ == '__main__' in Python
 //
 (function startGame() {
-  const word = 'hello';
+  const word = "hello";
 
   createDivsForChars(word);
   generateLetterButtons();
 
-  document.querySelectorAll('button').forEach((btn) => {
-    btn.addEventListener('click', (evt) => {
+  document.querySelectorAll("button").forEach((btn) => {
+    btn.addEventListener("click", (evt) => {
       const clickedBtn = evt.target;
       disableLetterButton(clickedBtn);
 
@@ -86,7 +114,7 @@ const resetGame = () => {
     });
   });
 
-  document.querySelector('#play-again').addEventListener('click', () => {
+  document.querySelector("#play-again").addEventListener("click", () => {
     resetGame();
   });
 })();
